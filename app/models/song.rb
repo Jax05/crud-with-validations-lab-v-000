95980a7068ca940c41403aaedbac054675616449
Carley Tripp <carley.tripp@outlook.com>
2019-02-26 03:03:05 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,7 +2,8 @@ class Song < ActiveRecord::Base
   validates :title, presence: true
   validates :title, uniqueness: { scope: [:artist_name, :release_year] }
   validates :released, inclusion: { in: [true, false] }
-  validates :release_year, numericality: { less_than_or_equal_to: DateTime.now.year }, allow_nil: true, if: :not_released?
+  validate :song_released_and_year_is_valid
+  # validates :release_year, numericality: { less_than_or_equal_to: DateTime.now.year }, allow_nil: true, if: :not_released?
   validates :artist_name, presence: true
 
   def song_released_and_year_is_valid
