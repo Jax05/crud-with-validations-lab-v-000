db1ac7c739605d1a8eca252b4d4355ffb50e741d
Carley Tripp <carley.tripp@outlook.com>
2019-02-26 03:14:34 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -7,8 +7,10 @@ class Song < ActiveRecord::Base
   validates :artist_name, presence: true
 
   def song_released_and_year_is_valid
-    if released == true
-      release_year <= Date.today.year
+    if released && release_year > DateTime.now.year
+      errors.add(:release_year, "cannot be in the future")
+    elsif released && release_year.nil?
+      errors.add(:release_year, "must include release year")
     end
   end
 end
